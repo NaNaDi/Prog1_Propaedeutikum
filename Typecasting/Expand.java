25c6162e87799e1c8b236a5065ec6c5e083481f3
Nana <nana.d@hotmail.de>
2017-09-18 13:33:40 +0200

    tasks for typecasting

---
+++ Typecasting/Expand.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//advanced
+
+public class Expand {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//todo: Als Eingabe dient eine Kombination aus Zeichen und Ganzzahlen (z.B. 2a3b5c)
+		//Im ersten Schritt beträgt die Ausgabe einen String, welcher so oft die entsprechenden Buchstaben
+		//ausgibt, wie die davor stehende Zahl es angibt. Für das obige Beispiel wäre die Ausgabe also:
+		//aabbbccccc
+		//Erweitere Deine Methode im nächsten Schritt so, dass auch mehrstellige Zahlen vor den Buchstaben stehen können,
+		//wie z.B.: 10a360b3c
+		//Im letzten Schritt sollten Zeichen, die nur ein Mal ausgegeben werden sollen, gar keine Zahl vorweg stehen haben
+		//z.B. a3b12cd5e -> abbbccccccccccccdeeeee
+		
+	}
+	
+}
